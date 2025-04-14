2a9b9c8254646c115ee61ee255d4891771b68404
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-04-14 10:44:21 +0530

    Create 1534.c

---
+++ 1501-2000/1534.c
@@ -0,0 +1,18 @@
+
+
+int countGoodTriplets(int* arr, int arrSize, int a, int b, int c){
+    int count = 0;
+    for(int i = 0; i < arrSize - 2; i++) {
+        for(int j = i + 1; j < arrSize - 1; j++) {
+            if(abs(arr[i] - arr[j]) > a) continue;
+            
+            for(int k = j + 1; k < arrSize; k++) {
+                if(abs(arr[i] - arr[k]) <= c 
+                    && abs(arr[j] - arr[k]) <= b)
+                    count++;        
+            }
+        }
+    }
+
+    return count;
+}
